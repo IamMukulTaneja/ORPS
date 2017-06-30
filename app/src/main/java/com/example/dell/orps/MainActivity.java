151e5876235f32ae5208320f5533688846e85f48
nitigya sharma <nitigya.sharma12@gmail.com>
2017-07-01 01:49:10 +0530

    options for vehicle removed and floating button added for direct booking' ' ; ' ' ' ' '

--- app/src/main/java/com/example/dell/orps/MainActivity.java
+++ app/src/main/java/com/example/dell/orps/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dell.orps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
