3dc143bbde076a345ba18a7c067800dd0068c683
Mukul <taneja.mukul@gmail.com>
2017-07-04 09:03:59 +0530

    ParkingDetails Activity Designing Done!

--- app/src/main/java/com/example/dell/orps/ParkingDetails.java
+++ app/src/main/java/com/example/dell/orps/ParkingDetails.java
@@ -3,7 +3,8 @@ package com.example.dell.orps;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.RequestQueue;
@@ -15,16 +16,26 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ParkingDetails extends AppCompatActivity {
+    ArrayAdapter<String> stationadapter;
+    ArrayAdapter<String> TwoWheeleradapter;
+    ArrayAdapter<String> FourWheeleradapter;
+    List<String> stationdetails,twowheelerlist,fourwheelerlist;
     String selectedstation;
-    TextView details;
-    String station_id,station_name,station_class,tot_2w_park,avail_2w_park,occ_2w_park,res_2w_park;
+    ListView station_details,two_wheeler,four_wheeler;
+    String station_id,station_name,station_class,tot_2w_park,avail_2w_park,tot_4w_park,avail_4w_park;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_details);
         Intent get = getIntent();
-        details = (TextView) findViewById(R.id.details);
+        station_details = (ListView)findViewById(R.id.stationdetails);
+        two_wheeler = (ListView)findViewById(R.id.Two_Wheeler);
+        four_wheeler = (ListView)findViewById(R.id.Four_Wheeler);
         if (get.hasExtra(Intent.EXTRA_TEXT)) {
             selectedstation = get.getStringExtra(Intent.EXTRA_TEXT);
         }
@@ -35,7 +46,12 @@ public class ParkingDetails extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 try {
                     station_id=response.getString("station_id");
-                    details.setText(station_id);
+                    station_name=response.getString("station_name");
+                    station_class=response.getString("station_class");
+                    tot_2w_park=response.getString("tot_2w_park");
+                    avail_2w_park=response.getString("avail_2w_park");
+                    tot_4w_park=response.getString("tot_4w_park");
+                    avail_4w_park=response.getString("avail_4w_park");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -44,7 +60,7 @@ public class ParkingDetails extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                details.setText("NAhi HUA"+error);
+
             }
         });
         queue.add(json);
@@ -52,5 +68,22 @@ public class ParkingDetails extends AppCompatActivity {
                3000,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        stationdetails= new ArrayList<>();
+        stationdetails.add("Station ID : "+station_id);
+        stationdetails.add("Station Name : "+station_name);
+        stationdetails.add("Station Class : "+station_class);
+        twowheelerlist= new ArrayList<>();
+        twowheelerlist.add("Total Parking : "+tot_2w_park);
+        twowheelerlist.add("Available Parking : "+avail_2w_park);
+        fourwheelerlist=new ArrayList<>();
+        fourwheelerlist.add("Total Parking : "+tot_4w_park);
+        fourwheelerlist.add("Available Parking : "+avail_4w_park);
+        stationadapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,stationdetails);
+        station_details.setAdapter(stationadapter);
+        TwoWheeleradapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,twowheelerlist);
+        two_wheeler.setAdapter(TwoWheeleradapter);
+        FourWheeleradapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,fourwheelerlist);
+        four_wheeler.setAdapter(FourWheeleradapter);
     }
+
 }
