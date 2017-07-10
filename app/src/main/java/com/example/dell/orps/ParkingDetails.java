daced7f4770f2d36da2b2c3ad08b39ed3fce316d
Mukul <taneja.mukul@gmail.com>
2017-07-11 02:36:37 +0530

    Data Recieved from server and showed to user using listview!

--- app/src/main/java/com/example/dell/orps/ParkingDetails.java
+++ app/src/main/java/com/example/dell/orps/ParkingDetails.java
@@ -3,14 +3,17 @@ package com.example.dell.orps;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -22,11 +25,13 @@ import java.util.List;
 
 public class ParkingDetails extends AppCompatActivity {
     ArrayAdapter<String> stationadapter;
-    ArrayAdapter<String> TwoWheeleradapter;
-    ArrayAdapter<String> FourWheeleradapter;
+    VolleyError e;
+    ArrayAdapter TwoWheeleradapter;
+    ArrayAdapter FourWheeleradapter;
     List<String> stationdetails,twowheelerlist,fourwheelerlist;
     String selectedstation;
     ListView station_details,two_wheeler,four_wheeler;
+    Button book;
     String station_id,station_name,station_class,tot_2w_park,avail_2w_park,tot_4w_park,avail_4w_park;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,56 +39,62 @@ public class ParkingDetails extends AppCompatActivity {
         setContentView(R.layout.activity_parking_details);
         Intent get = getIntent();
         station_details = (ListView)findViewById(R.id.stationdetails);
+        book=(Button)findViewById(R.id.book);
         two_wheeler = (ListView)findViewById(R.id.Two_Wheeler);
         four_wheeler = (ListView)findViewById(R.id.Four_Wheeler);
         if (get.hasExtra(Intent.EXTRA_TEXT)) {
             selectedstation = get.getStringExtra(Intent.EXTRA_TEXT);
         }
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.43.143/OPRS-server-master/getStationParkingStatus.php";
-        JsonObjectRequest json = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+        String url = "http://192.168.1.103/OPRS-server-master/getStationParkingStatus.php";
+        StringRequest json=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onResponse(String response) {
                 try {
-                    station_id=response.getString("station_id");
-                    station_name=response.getString("station_name");
-                    station_class=response.getString("station_class");
-                    tot_2w_park=response.getString("tot_2w_park");
-                    avail_2w_park=response.getString("avail_2w_park");
-                    tot_4w_park=response.getString("tot_4w_park");
-                    avail_4w_park=response.getString("avail_4w_park");
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    JSONObject jsonobject=new JSONObject(response);
+                    station_id=jsonobject.get("station_id").toString();
+                    station_name=jsonobject.get("station_name").toString();
+                    station_class=jsonobject.get("station_class").toString();
+                    tot_2w_park=jsonobject.get("tot_2w_park").toString();
+                    avail_2w_park=jsonobject.get("avail_2w_park").toString();
+                    tot_4w_park=jsonobject.get("tot_4w_park").toString();
+                    avail_4w_park=jsonobject.get("avail_4w_park").toString();
+                    stationdetails= new ArrayList<>();
+                    stationdetails.add("Station ID : "+station_id);
+                    stationdetails.add("Station Name : "+station_name);
+                    stationdetails.add("Station Class : "+station_class);
+                    stationadapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,stationdetails);
+                    station_details.setAdapter(stationadapter);
+                    twowheelerlist= new ArrayList<>();
+                    twowheelerlist.add("Total Parking : "+tot_2w_park);
+                    twowheelerlist.add("Available Parking : "+avail_2w_park);
+                    fourwheelerlist=new ArrayList<>();
+                    fourwheelerlist.add("Total Parking : "+tot_4w_park);
+                    fourwheelerlist.add("Available Parking : "+avail_4w_park);
+                    TwoWheeleradapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,twowheelerlist);
+                    two_wheeler.setAdapter(TwoWheeleradapter);
+                    FourWheeleradapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,fourwheelerlist);
+                    four_wheeler.setAdapter(FourWheeleradapter);
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
                 }
+
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+e=error;
             }
         });
         queue.add(json);
-        json.setRetryPolicy(new DefaultRetryPolicy(
-               3000,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        stationdetails= new ArrayList<>();
-        stationdetails.add("Station ID : "+station_id);
-        stationdetails.add("Station Name : "+station_name);
-        stationdetails.add("Station Class : "+station_class);
-        twowheelerlist= new ArrayList<>();
-        twowheelerlist.add("Total Parking : "+tot_2w_park);
-        twowheelerlist.add("Available Parking : "+avail_2w_park);
-        fourwheelerlist=new ArrayList<>();
-        fourwheelerlist.add("Total Parking : "+tot_4w_park);
-        fourwheelerlist.add("Available Parking : "+avail_4w_park);
-        stationadapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,stationdetails);
-        station_details.setAdapter(stationadapter);
-        TwoWheeleradapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,twowheelerlist);
-        two_wheeler.setAdapter(TwoWheeleradapter);
-        FourWheeleradapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,fourwheelerlist);
-        four_wheeler.setAdapter(FourWheeleradapter);
-    }
 
+
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ParkingDetails.this, ""+station_id, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
